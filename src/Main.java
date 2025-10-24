740e63cf640d4b448d78d34b04f1b3e40693bdfa
davidl572 <davidl572@nycstudents.net>
2025-10-24 15:03:39 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -9,8 +9,11 @@ public class Main
         for(int i = 30; i < 44; i++) schedule[1][i] = true;
         for(int i = 50; i < 59; i++) schedule[1][i] = true;
             a.findFreeBlock(2,15);
+            a.findFreeBlock(2,9);
+            a.findFreeBlock(2,20);
+            
             schedule = new boolean[8][60];
-        for(int i = 25; i < 30; i++) schedule[1][i] = true;
+         for(int i = 25; i < 30; i++) schedule[1][i] = true;
         for(int i = 0; i < 15; i++) schedule[2][i] = true;
         for(int i = 41; i < 60; i++) schedule[2][i] = true;
         for(int i = 5; i < 30; i++) schedule[3][i] = true;
